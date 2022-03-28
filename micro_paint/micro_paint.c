834937d7c81eba88966d65a491739920275a7dbc
Francesco Drudi <fdrudi@e3r10p4.42roma.it>
2022-03-28 17:13:00 +0200

    update_28/03_2

--- micro_paint/micro_paint.c
+++ micro_paint/micro_paint.c
@@ -6,7 +6,7 @@
 /*   By: fdrudi <fdrudi@student.42roma.it>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/28 12:25:43 by fdrudi            #+#    #+#             */
-/*   Updated: 2022/03/28 16:41:30 by fdrudi           ###   ########.fr       */
+/*   Updated: 2022/03/28 17:11:28 by fdrudi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,7 +49,8 @@ int	ft_read(t_paint *p, int def)
 	}
 	else
 	{
-		if (fscanf(p->fp, "%s %f %f %f %f %s", p->r, &p->p_x, &p->p_y, &p->x, &p->y, p->c2) < 6 || p->c2[1] != '\0' || p->r[1] != '\0')
+		if (fscanf(p->fp, "%s %f %f %f %f %s", p->r, &p->p_x, &p->p_y, &p->x, &p->y, p->c2) < 6
+			|| p->c2[1] != '\0' || p->r[1] != '\0')
 		{
 			write(1, "Error: Operation file corrupted\n", 32);
 			return (1);
@@ -69,25 +70,17 @@ void	ft_print_map(t_paint *p)
 	int	y;
 	int	x;
 
-	x = 0;
-	y = 0;
-	while (y < p->w_y)
+	y = -1;
+	while (++y < p->w_y)
 	{
-		x = 0;
-		while (x < p->w_x)
-		{
+		x = -1;
+		while (++x < p->w_x)
 			write(1, &p->map[y][x], 1);
-			x++;
-		}
 		write(1, "\n", 1);
-		y++;
 	}
-	write(1, "\n", 1);
-	write(1, "\n", 1);
-	write(1, "\n", 1);
 }
 
-void	ft_window(t_paint *p)
+int	ft_window(t_paint *p)
 {
 	int	i;
 
@@ -99,6 +92,18 @@ void	ft_window(t_paint *p)
 		memset(p->map[i], p->c[0], p->w_x);
 		i++;
 	}
+	if (ft_read(&p, -1) == 1)
+		return (1);
+	if (p->r[0] == 'R')
+		ft_square(&p);
+	else if (p->r[0] == 'r')
+		ft_square2(&p);
+	else
+	{
+		write(1, "Error: Operation file corrupted\n", 32);
+		return (1);
+	}
+	return (0);
 }
 
 void	ft_square(t_paint *p)
@@ -157,18 +162,8 @@ int	main(int argc, char **argv)
 	p.fp = fopen(argv[1], "r");
 	if (ft_read(&p, 1) == 1)
 		return (1);
-	ft_window(&p);
-	if (ft_read(&p, -1) == 1)
+	if (ft_window(&p) == 1)
 		return (1);
-	if (p.r[0] == 'R')
-		ft_square(&p);
-	else if (p.r[0] == 'r')
-		ft_square2(&p);
-	else
-	{
-		write(1, "Error: Operation file corrupted\n", 32);
-		return (1);
-	}
 	ft_print_map(&p);
 	fclose(p.fp);
 	return (0);
